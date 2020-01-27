3641dc1c6afd8594de17d8e66b772444aff6a1c6
Ryan Benech <rbenech@roos.com>
2020-01-27 10:03:58 -0500

    remove gmaps sensor line (no longer required)

--- js/google-map.js
+++ js/google-map.js
@@ -1,14 +1,13 @@
-
+// NOT USED, got snip from google maps API wizard
 var google;
 
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+    var myLatlng = new google.maps.LatLng(37.2922625, -121.9126747);
+    //  https://www.google.com/maps/place/Coldwell+Banker+Residential+Brokerage+-+San+Jose-Willow+Glen/@37.2922625,-121.9126747
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 7,
@@ -34,7 +33,7 @@ function init() {
         ]
     };
 
-    
+
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
@@ -42,11 +41,11 @@ function init() {
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
+
+    var addresses = ['Coldwell Banker Residential Brokerage - San Jose-Willow Glen'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x], null, function (data) {
             var p = data.results[0].geometry.location
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
@@ -57,6 +56,6 @@ function init() {
 
         });
     }
-    
+
 }
 google.maps.event.addDomListener(window, 'load', init);
